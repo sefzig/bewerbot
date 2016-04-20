ff7b87171b4eee65dbd183058b93737bf8ea6bdb
Andreas Sefzig <andreas@sefzig.net>
2016-04-20 12:30:00 +0200

    Express Views Js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(bodyParser.json());
 app.use("/css", express.static(__dirname + '/views/css'));
+app.use("/js", express.static(__dirname + '/views/js'));
 
 app.get('/', function(req, res) {
     res.render('index', {
