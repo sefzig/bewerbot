e706b40666f540a724d325090d2d2042f3da0383
Andreas Sefzig <andreas@sefzig.net>
2016-04-20 13:23:56 +0200

    Test Esthers Script.json

--- script.js
+++ script.js
@@ -1,36 +1,78 @@
 'use strict';
 
+const _ = require('lodash');
 const Script = require('smooch-bot').Script;
+
+const scriptRules = require('./script.json');
 
 module.exports = new Script({
     processing: {
-        prompt: (bot) => bot.say('Beep boop...'),
+        //prompt: (bot) => bot.say('Beep boop...'),
         receive: () => 'processing'
     },
 
     start: {
         receive: (bot) => {
-            return bot.say('Hi! I\'m Smooch Bot!')
-                .then(() => 'askName');
+            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+                .then(() => 'speak');
         }
     },
 
-    askName: {
-        prompt: (bot) => bot.say('What\'s your name?'),
+    speak: {
         receive: (bot, message) => {
-            const name = message.text;
-            return bot.setProp('name', name)
-                .then(() => bot.say(`Great! I'll call you ${name}`))
-                .then(() => 'finish');
-        }
-    },
 
-    finish: {
-        receive: (bot, message) => {
-            return bot.getProp('name')
-                .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
-                        'teach me how to do anything else!'))
-                .then(() => 'finish');
+            let upperText = message.text.trim().toUpperCase();
+
+            function updateSilent() {
+                switch (upperText) {
+                    case "CONNECT ME":
+                        return bot.setProp("silent", true);
+                    case "DISCONNECT":
+                        return bot.setProp("silent", false);
+                    default:
+                        return Promise.resolve();
+                }
+            }
+
+            function getSilent() {
+                return bot.getProp("silent");
+            }
+
+            function processMessage(isSilent) {
+                if (isSilent) {
+                    return Promise.resolve("speak");
+                }
+
+                if (!_.has(scriptRules, upperText)) {
+                    return bot.say(`I didn't understand that.`).then(() => 'speak');
+                }
+
+                var response = scriptRules[upperText];
+                var lines = response.split(/(<img src=\'[^>]*\'\/>)/);
+
+                var p = Promise.resolve();
+                _.each(lines, function(line) {
+                    line = line.trim();
+                    if (!line.startsWith("<")) {
+                        p = p.then(function() {
+                            return bot.say(line);
+                        });
+                    } else {
+                        // p = p.then(function() {
+                        //     var start = line.indexOf("'") + 1;
+                        //     var end = line.lastIndexOf("'");
+                        //     var imageFile = line.substring(start, end);
+                        //     return bot.sendImage(imageFile);
+                        // });
+                    }
+                })
+
+                return p.then(() => 'speak');
+            }
+
+            return updateSilent()
+                .then(getSilent)
+                .then(processMessage);
         }
     }
 });
